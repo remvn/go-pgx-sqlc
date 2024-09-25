4661ca92ba3297577cdc6e5fb5b7ef5b0387acca
remvn <34063162+remvn@users.noreply.github.com>
2024-09-25 22:35:43 +0700

    wip

--- database/database_test.go
+++ database/database_test.go
@@ -3,10 +3,14 @@ package database_test
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/remvn/go-pgx-sqlc/database"
+	"github.com/remvn/go-pgx-sqlc/database/sqlc"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -19,8 +23,10 @@ func createContainer() (*postgres.PostgresContainer, error) {
 	dbPassword := "password"
 	dbName := "testing"
 
+	schemaFile := filepath.Join("../sqlc/", "schema.sql")
 	postgresContainer, err := postgres.Run(ctx,
 		"docker.io/postgres:16-alpine",
+		postgres.WithInitScripts(schemaFile),
 		postgres.WithDatabase(dbName),
 		postgres.WithUsername(dbUsername),
 		postgres.WithPassword(dbPassword),
@@ -36,15 +42,78 @@ func createContainer() (*postgres.PostgresContainer, error) {
 	return postgresContainer, nil
 }
 
-func TestDatabase(t *testing.T) {
+func createTestDb() (*database.Database, error) {
 	ctx := context.Background()
 	container, err := createContainer()
-	assert.NoError(t, err, "unable to create container")
+	if err != nil {
+		return nil, err
+	}
 
 	connStr, err := container.ConnectionString(ctx)
-	assert.NoError(t, err, "unable to get connStr")
+	if err != nil {
+		return nil, err
+	}
 
 	db := database.NewDatabase(connStr)
 	err = db.Pool.Ping(ctx)
-	assert.NoError(t, err, "Unable to ping the database.")
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func TestSqlc(t *testing.T) {
+	ctx := context.Background()
+	db, err := createTestDb()
+	assert.NoError(t, err, "unable to create test database")
+
+	author, err := db.Query.CreateAuthor(ctx, sqlc.CreateAuthorParams{
+		Name: "Rob Pike",
+	})
+	assert.NoError(t, err, "unable to create author")
+	t.Logf("created author %s with id %d\n", author.Name, author.ID)
+
+	authors, err := db.Query.ListAuthors(ctx)
+	assert.NoError(t, err, "unable to get author lists")
+	t.Logf("author table has %d row\n", len(authors))
+}
+
+func TestPgx(t *testing.T) {
+	db, err := createTestDb()
+	assert.NoError(t, err, "unable to create test database")
+
+	_, err = pgxInsert(db, "Rob Pike", pgtype.Text{})
+	assert.NoError(t, err, "unable to create author")
+}
+
+func pgxInsert(db *database.Database, name string, bio pgtype.Text) (any, error) {
+	query := `INSERT INTO author (name, bio) VALUES (@name, @bio) RETURNING *`
+	args := pgx.NamedArgs{
+		"name": name,
+		"bio":  bio,
+	}
+	row := db.Pool.QueryRow(context.Background(), query, args)
+	author := &sqlc.Author{}
+	err := row.Scan(author)
+	if err != nil {
+		return nil, err
+	}
+
+	return author, nil
+}
+
+func pgxSelect(db *database.Database, id int) (any, error) {
+	query := `SELECT name, bio from author where id = @id`
+	args := pgx.NamedArgs{
+		"id": id,
+	}
+	row := db.Pool.QueryRow(context.Background(), query, args)
+	author := &sqlc.Author{}
+	err := row.Scan(author)
+	if err != nil {
+		return nil, err
+	}
+
+	return author, nil
 }
